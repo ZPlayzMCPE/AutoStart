de95a0b3d3f0887f4531b54ae1d77f0a61816234
David <30806420+diamondgamermcpe@users.noreply.github.com>
2018-01-15 22:00:54 -0500

    Better display

--- src/autostart/task/RestartTask.php
+++ src/autostart/task/RestartTask.php
@@ -10,29 +10,37 @@ use pocketmine\plugin\Plugin;
 
 class RestartTask extends PluginTask {
 
-	public $minutes = 0;
+	public $seconds = 0;
 
 	public function __construct(Plugin $plugin){
 		$this->plugin = $plugin;
 		parent::__construct($plugin);
-
 	}
 
 	public function onRun($tick) {
-		$time = $this->plugin->resource->get("Time");
-		$this->minutes++;
-		$restarttime = $time - $this->minutes;
+		$time = $this->plugin->resource->get("Time")*60;
+		$this->seconds++;
+		$restartTime = $time - $this->seconds;
+		$currentTime = intdiv($restartTime, 60);
 		foreach($this->plugin->getServer()->getOnlinePlayers() as $player) {
-			$popup = str_replace("{time}", $restarttime, $this->plugin->resource->get("Popup"));
+			$popup = str_replace("{time}", $currentTime, $this->plugin->resource->get("Popup"));
 			$player->sendPopup($popup);
 		}
-		if($restarttime == 1) {
+		if($restartTime/60 === 1) {
 			foreach($this->plugin->getServer()->getOnlinePlayers() as $player) {
-				$message = str_replace("{time}", $restarttime, $this->plugin->resource->get("Popup"));
-				$player->addTitle($message, "", 60, 60, 60);
+				$message = str_replace("{time}", $currentTime, $this->plugin->resource->get("Popup"));
+				$title = str_replace("minutes", "minute", $message);
+				$player->addTitle($title, "", 20, 40, 20);
 			}
 		}
-		if($restarttime == 0) {
+		if($restartTime <= 60) {
+			foreach($this->plugin->getServer()->getOnlinePlayers() as $player) {
+				$message = str_replace("{time}", $restartTime, $this->plugin->resource->get("Popup"));
+				$popup = str_replace("minutes", "", $message);
+				$player->sendPopup($popup);
+			}
+		}
+		if($restartTime === 0) {
 			foreach($this->plugin->getServer()->getOnlinePlayers() as $player) {
 				$reason = $this->plugin->resource->get("Kick Message");
 				$player->kick($reason);
