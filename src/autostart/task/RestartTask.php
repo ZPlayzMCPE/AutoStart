147788276dfb1dbf94ad828c28ead520e2580043
David <30806420+diamondgamermcpe@users.noreply.github.com>
2018-01-15 22:23:33 -0500

    Added seconds

--- src/autostart/task/RestartTask.php
+++ src/autostart/task/RestartTask.php
@@ -18,17 +18,18 @@ class RestartTask extends PluginTask {
 	}
 
 	public function onRun($tick) {
-		$time = $this->plugin->resource->get("Time")*60;
+		$time = $this->plugin->resource->get("Time") * 60;
 		$this->seconds++;
 		$restartTime = $time - $this->seconds;
 		$currentTime = intdiv($restartTime, 60);
+		$currentSeconds = $restartTime - $currentTime * 60;
 		foreach($this->plugin->getServer()->getOnlinePlayers() as $player) {
-			$popup = str_replace("{time}", "$currentTime minutes", $this->plugin->resource->get("Popup"));
+			$popup = str_replace("{time}", "$currentTime minutes $currentSeconds seconds", $this->plugin->resource->get("Popup"));
 			$player->sendPopup($popup);
 		}
 		if($currentTime === 1) {
 			foreach($this->plugin->getServer()->getOnlinePlayers() as $player) {
-				$popup = str_replace("{time}", "$currentTime minute", $this->plugin->resource->get("Popup"));
+				$popup = str_replace("{time}", "$currentTime minute $currentSeconds seconds", $this->plugin->resource->get("Popup"));
 				$player->sendPopup($popup);
 			}
 		}
@@ -38,7 +39,7 @@ class RestartTask extends PluginTask {
 				$player->addTitle($title, "", 20, 40, 20);
 			}
 		}
-		if($restartTime <= 60) {
+		if($restartTime <= 59) {
 			foreach($this->plugin->getServer()->getOnlinePlayers() as $player) {
 				$popup = str_replace("{time}", "$restartTime seconds", $this->plugin->resource->get("Popup"));
 				$player->sendPopup($popup);
