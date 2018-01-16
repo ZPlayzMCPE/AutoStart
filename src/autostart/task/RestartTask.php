72d6585b14c4252e31b00493a79aeafaf520a070
David <30806420+diamondgamermcpe@users.noreply.github.com>
2018-01-15 22:16:29 -0500

    Update RestartTask.php

--- src/autostart/task/RestartTask.php
+++ src/autostart/task/RestartTask.php
@@ -23,20 +23,24 @@ class RestartTask extends PluginTask {
 		$restartTime = $time - $this->seconds;
 		$currentTime = intdiv($restartTime, 60);
 		foreach($this->plugin->getServer()->getOnlinePlayers() as $player) {
-			$popup = str_replace("{time}", $currentTime, $this->plugin->resource->get("Popup"));
+			$popup = str_replace("{time}", "$currentTime minutes", $this->plugin->resource->get("Popup"));
 			$player->sendPopup($popup);
+		}
+		if($currentTime === 1) {
+			foreach($this->plugin->getServer()->getOnlinePlayers() as $player) {
+				$popup = str_replace("{time}", "$currentTime minute", $this->plugin->resource->get("Popup"));
+				$player->sendPopup($popup);
+			}
 		}
 		if($restartTime/60 === 1) {
 			foreach($this->plugin->getServer()->getOnlinePlayers() as $player) {
-				$message = str_replace("{time}", $currentTime, $this->plugin->resource->get("Popup"));
-				$title = str_replace("minutes", "minute", $message);
+				$title = str_replace("{time}", "$currentTime minute", $this->plugin->resource->get("Popup"));
 				$player->addTitle($title, "", 20, 40, 20);
 			}
 		}
 		if($restartTime <= 60) {
 			foreach($this->plugin->getServer()->getOnlinePlayers() as $player) {
-				$message = str_replace("{time}", $restartTime, $this->plugin->resource->get("Popup"));
-				$popup = str_replace("minutes", "", $message);
+				$popup = str_replace("{time}", "$restartTime seconds", $this->plugin->resource->get("Popup"));
 				$player->sendPopup($popup);
 			}
 		}
